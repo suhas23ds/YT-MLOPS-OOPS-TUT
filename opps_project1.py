6c4a755b657723f6db6bc70a60105456c7377764
Suhas <suhaspawar23@gmail.com>
2026-01-08 21:31:10 +0530

    Signup and login feature done

--- opps_project1.py
+++ opps_project1.py
@@ -9,9 +9,9 @@ class chatbook:
         user_input=input("welcome to chatbook\n1. Sign Up\n2. Log In\n3. write a post\n4. to messages\n5. exit\n")
     
         if user_input=='1':
-            pass
+            self.sign_up()
         elif user_input=='2':
-            pass
+            self.log_in()
         elif user_input=='3':
             pass
         elif user_input=='4':
@@ -19,4 +19,21 @@ class chatbook:
         else:
             exit()
 
+    def sign_up(self):
+        email=input("Please enter your email: ")
+        password=input("Please enter your password: ")
+        self.username=email
+        self.password=password
+        print("Sign up successful!") 
+        self.menu()
+
+    def log_in(self):
+        email=input("Enter your email: ")
+        password=input("Enter your password: ")
+        if email==self.username and password==self.password:
+            self.loggedin=True
+            print("Log in successful!")
+        else:
+            print("Invalid credentials.")
+        self.menu()
 obj=chatbook()
